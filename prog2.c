ac4464573029d16acc2afc133e1302917974f6bd
Margret-Jaison <77883030+Margret-Jaison@users.noreply.github.com>
2021-06-30 20:50:23 +0530

    sum of 2 numbers

---
+++ prog2.c
@@ -0,0 +1,12 @@
+// sum of 2 numbers
+#include<stdio.h>
+int main()
+{ 
+    int x,y,z;
+    printf("enter the 2 numbers to add\n");
+    
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("sum is %d\n",z);
+}
+    
