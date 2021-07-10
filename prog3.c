d0f9f4775a7581258a6f4850b17f24739ad674ca
Margret-Jaison <77883030+Margret-Jaison@users.noreply.github.com>
2021-07-10 15:54:31 +0530

    odd or even no.

---
+++ prog3.c
@@ -0,0 +1,17 @@
+// to print no is odd or even
+#include <stdio.h>
+
+int main() {
+    int n; 
+    printf("enter a number");
+    scanf("%d",&n);
+if(n%2==0)
+{
+    printf("the %d is even\n",n);
+}
+else
+{
+    printf("the %d is odd\n",n);
+}
+    
+}
